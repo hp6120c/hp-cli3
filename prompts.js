1bc51a4b63748337963087cf9954f5703bf8b62a
hp6120c <249908834@qq.com>
2020-01-16 17:28:44 +0800

    update

--- prompts.js
+++ prompts.js
@@ -1,6 +1,16 @@
 module.exports = [
   {
     type: 'list', // 即类型为 选择项
+    name: 'typeScript', // 名称，作为下面 generator 函数 options 的键
+    message: '是否使用typeScript', // 提示语
+    choices: [
+      { name: '是', value: 'true' },
+      { name: '否', value: 'false' }
+    ],
+    default: 'false',
+  },
+  {
+    type: 'list', // 即类型为 选择项
     name: 'file_saver', // 名称，作为下面 generator 函数 options 的键
     message: '是否需要file-saver来保存文件（产品有下载功能时用）', // 提示语
     choices: [
--- template/src/main.js
+++ template/src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
-<%_ if (options.echarts) { _%>
+<%_ if (options.echarts=='true') { _%>
 import echarts from 'echarts';
     <%_ } _%>
 import App from './App.vue';
@@ -9,7 +9,7 @@ import router from './router';
 import store from './store';
 import http from './api/api';
 
-<%_ if (options.echarts) { _%>
+<%_ if (options.echarts=='true') { _%>
 
 Vue.prototype.$echarts = echarts;
       <%_ } _%>
